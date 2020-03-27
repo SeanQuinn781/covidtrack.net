0cd995571ed3cc9b642973360367f0ff92574cce
root <1-root@users.noreply.localhost.com>
2020-03-27 17:23:00 -0400

    mobile nav

--- client/src/App.js
+++ client/src/App.js
@@ -137,8 +137,20 @@ class App extends React.Component {
           </div>
         </div>
         </a>
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon" />
+        
+        <button 
+          className="navbar-toggler" 
+          type="button" 
+          data-toggle="collapse" 
+          data-target="#navbarSupportedContent" 
+          aria-controls="navbarSupportedContent" 
+          aria-expanded="false" 
+          aria-label="Toggle navigation"
+          style={{color:'#fff !important;'}}
+        >
+          <span className="navbar-toggler-icon"> 
+            <i className="fas fa-bars fa-1x"></i>
+          </span>
         </button>
 
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
