4538bdbd4e78e66c2f30a8593bc68a7ac8cf1a4c
seanquinn.io <seanquinn.io>
2021-01-18 12:21:09 -0500

    Update dependencies

--- client/src/maps/UnitedStatesCountyMap.js
+++ client/src/maps/UnitedStatesCountyMap.js
@@ -148,76 +148,79 @@ class UnitedStatesCountyMap extends React.Component {
 
 
     return (
-      <ComposableMap 
-        projection="geoAlbersUsa"
-        id="unitedStatesCountyMap"
-      >
-        <Geographies 
-          geography="counties-10m.json">
-          {({ geographies }) => (
-            <>
-              {geographies.map((geo, i) => {
-                
-                let countyData;
-                let relativeIndex;
+      <div>
+        <h3 id="countyMapInstructions">For the best experience view this map on a desktop and hover over states/counties to view local Covid19 statistics</h3>
+        <ComposableMap
+          projection="geoAlbersUsa"
+          id="unitedStatesCountyMap"
+        >
+          <Geographies
+            geography="counties-10m.json">
+            {({ geographies }) => (
+              <>
+                {geographies.map((geo, i) => {
 
-                // There is no single field in the covid county data that can be used to match it with
-                // the geography currently being iterated over. This is a workaround/hack to match geo and data 
-                // using name first (many counties share names regardless of what state they're in) and coordinates
-                const centroid = geoCentroid(geo);
-                const geoLat = Math.round(centroid[1]);
-                const geoLong = Math.round(centroid[0]);
+                  let countyData;
+                  let relativeIndex;
 
-                // iterate all data to find current geography
-                unitedStatesCountyData.forEach((c,index) => {
-                  // match on county name
-                  if(c.county === geo.properties.name) {
-                    // due to many counties with the same name, also match on long/lat
-                    if(Math.round(c.coordinates.longitude) ===  Math.round(geoLong))
-                      if (Math.round(c.coordinates.latitude)  ===  Math.round(geoLat))
-                        countyData = c;
-                    relativeIndex = index;
+                  // There is no single field in the covid county data that can be used to match it with
+                  // the geography currently being iterated over. This is a workaround/hack to match geo and data
+                  // using name first (many counties share names regardless of what state they're in) and coordinates
+                  const centroid = geoCentroid(geo);
+                  const geoLat = Math.round(centroid[1]);
+                  const geoLong = Math.round(centroid[0]);
+
+                  // iterate all data to find current geography
+                  unitedStatesCountyData.forEach((c,index) => {
+                    // match on county name
+                    if(c.county === geo.properties.name) {
+                      // due to many counties with the same name, also match on long/lat
+                      if(Math.round(c.coordinates.longitude) ===  Math.round(geoLong))
+                        if (Math.round(c.coordinates.latitude)  ===  Math.round(geoLat))
+                          countyData = c;
+                      relativeIndex = index;
+                    }
+                  })
+
+                  if(countyData === undefined ) {
+                    console.log('no matching location data found for ')
+                    return this.defaultGeography(geo,centroid);
                   }
-                })
-                
-                if(countyData === undefined ) {
-                  console.log('no matching location data found for ')
-                  return this.defaultGeography(geo,centroid);
-                }
-    
-                let stateColor;
-                // assign the state a color based on the value of total cases or deaths heatmap
-                if (renderCasualtiesHeatmap) {
-                  stateColor  = countyColorPalette(unitedStatesCountyData, relativeIndex, 'deaths');
-                } else if(renderCasesHeatmap) {
-                  stateColor  = countyColorPalette(unitedStatesCountyData, relativeIndex, 'confirmed')
-                } else {
-                  stateColor = randomGeographyColor();
-                }
 
-                return (
-                  <>
-                    <Geography
-                      className="locationData"
-                      key={geo.rsmKey}
-                      onMouseMove={(e,props) => this.handleMouseMove(e, countyData)}
-                      onMouseLeave={this.handleMouseLeave}
-                      style={{
-                        hover: { fill: "darkgray" },
-                        pressed: { fill: "#aaa", outline: "none" },
-                      }}
-                      fill={stateColor}
-                      stroke="#fff"
-                      geography={geo}
-                    />
-                    <g key={`${geo.rsmKey}-name`} />
-                  </>
-                );
-              })}
-            </>
-          )} 
-        </Geographies>
-      </ComposableMap>
+                  let stateColor;
+                  // assign the state a color based on the value of total cases or deaths heatmap
+                  if (renderCasualtiesHeatmap) {
+                    stateColor  = countyColorPalette(unitedStatesCountyData, relativeIndex, 'deaths');
+                  } else if(renderCasesHeatmap) {
+                    stateColor  = countyColorPalette(unitedStatesCountyData, relativeIndex, 'confirmed')
+                  } else {
+                    stateColor = randomGeographyColor();
+                  }
+
+                  return (
+                    <>
+                      <Geography
+                        className="locationData"
+                        key={geo.rsmKey}
+                        onMouseMove={(e,props) => this.handleMouseMove(e, countyData)}
+                        onMouseLeave={this.handleMouseLeave}
+                        style={{
+                          hover: { fill: "darkgray" },
+                          pressed: { fill: "#aaa", outline: "none" },
+                        }}
+                        fill={stateColor}
+                        stroke="#fff"
+                        geography={geo}
+                      />
+                      <g key={`${geo.rsmKey}-name`} />
+                    </>
+                  );
+                })}
+              </>
+            )}
+          </Geographies>
+        </ComposableMap>
+      </div>
     )
   }
 };
